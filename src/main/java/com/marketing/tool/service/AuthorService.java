e4dba1895f4962f129e069f7257bac3e08bb37ad
sri <sri@192.168.0.5>
2016-05-07 19:13:34 +0530

    changes from anil

--- src/main/java/com/marketing/tool/service/AuthorService.java
+++ src/main/java/com/marketing/tool/service/AuthorService.java
@@ -1,16 +1,15 @@
 package com.marketing.tool.service;
 
-<<<<<<< HEAD
-import com.marketing.tool.domain.EditReports;
-=======
->>>>>>> branch 'develop' of https://aneelkumar_ravula@bitbucket.org/aneelkumar_ravula/tool.git
 import java.util.List;
 
 import com.marketing.tool.domain.Author;
+import com.marketing.tool.domain.AuthorReportsResp;
 import com.marketing.tool.domain.Keyskills;
 
 public interface AuthorService extends UserService {
-	public EditReports getAuthorReports(int userId, int pageNumber, int results);
+	public AuthorReportsResp getAuthorReports(int userId, int pageNumber, int results);
 
 	public List<Author> findAuhtors(String country, List<Keyskills> skills);
+	public void getAuthorPublishedReports(int userId, int pageNumber, int results, AuthorReportsResp resp) ;
+	public void getAuthorReportsList(int userId, int pageNumber, int results, AuthorReportsResp resp);
 }
--- src/main/java/com/marketing/tool/service/AuthorServiceImpl.java
+++ src/main/java/com/marketing/tool/service/AuthorServiceImpl.java
@@ -1,5 +1,6 @@
 package com.marketing.tool.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -7,16 +8,24 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
 import com.marketing.tool.domain.Author;
+import com.marketing.tool.domain.AuthorReportsResp;
 import com.marketing.tool.domain.Keyskills;
+import com.marketing.tool.domain.ReportForm;
 import com.marketing.tool.domain.User;
 import com.marketing.tool.domain.UserProfileType;
+import com.marketing.tool.reponse.builder.ReportsSearchResponseBuilder;
 import com.marketing.tool.repository.AuthorRepository;
+import com.marketing.tool.repository.ReportRepository;
 import com.marketing.tool.repository.RoleRepository;
+import com.marketing.tool.utility.ReportStatusEnum;
+import com.marketing.tool.vo.ReportVo;
 
 @Service
 @Validated
@@ -27,6 +36,12 @@ public class AuthorServiceImpl extends UserServiceImpl implements AuthorService 
 	
 	@Autowired
 	AuthorRepository authorRepository;
+	
+	@Autowired	
+	private ReportRepository reportRepository;
+	
+	
+	
 	
 	@Inject
 	public AuthorServiceImpl(AuthorRepository repository) {
@@ -48,4 +63,61 @@ public class AuthorServiceImpl extends UserServiceImpl implements AuthorService 
 		}
 		return authorRepository.findByCountryAndKeyskillsIn(country, skills);
 	}
+
+
+	@Override
+	public AuthorReportsResp getAuthorReports(int userId, int pageNumber, int results) {
+		AuthorReportsResp  resp = new AuthorReportsResp();
+		
+		getAuthorPublishedReports(userId, pageNumber, results,resp);
+		
+		//caluclate monthly basis based on date
+				
+		//other reports
+		 getAuthorReportsList(userId, pageNumber, results,resp);
+		
+		
+		return resp;
+	}
+	
+	public void getAuthorPublishedReports(int userId, int pageNumber, int results, AuthorReportsResp resp) {
+		//build page request
+		final PageRequest page = new PageRequest(pageNumber, results);
+
+		//pulished reports
+		List<Integer> published = new ArrayList<>();
+		published.add(ReportStatusEnum.PUBLISHED.getValue());
+		
+		Page<ReportForm> publishedReports =reportRepository.findAuthorReports(userId, published, page);
+		if(publishedReports != null  && pageNumber == 0) {
+			resp.setPubRepCount(publishedReports.getTotalPages());
+		}
+		
+		List<ReportVo> pubReVo =ReportsSearchResponseBuilder.buildReports(publishedReports.getContent());
+		
+		resp.setPulishedReports(pubReVo);
+		
+		//return pubReVo;
+	}
+	public void getAuthorReportsList(int userId, int pageNumber, int results, AuthorReportsResp resp) {
+		//build page request
+		final PageRequest page = new PageRequest(pageNumber, results);
+
+		//pulished reports
+		List<Integer> others = new ArrayList<>();
+		others.add(ReportStatusEnum.PUBLISHER_PENDING.getValue());
+		others.add(ReportStatusEnum.CREATED.getValue());
+		others.add(ReportStatusEnum.REVIWER_COMMENTS.getValue());
+		others.add(ReportStatusEnum.REVIWER_PENDING.getValue());
+		
+		Page<ReportForm> publishedReports =reportRepository.findAuthorReports(userId, others, page);
+		if(publishedReports != null && pageNumber == 0) {
+			resp.setOtherrepCount(publishedReports.getTotalPages());
+		}
+		List<ReportVo> pubReVo =ReportsSearchResponseBuilder.buildReports(publishedReports.getContent());
+		resp.setRepList(pubReVo);
+	}
+	
+	//Build Graphs
+	
 }
